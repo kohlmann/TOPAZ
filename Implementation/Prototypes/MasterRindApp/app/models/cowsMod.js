95bd8871e937b6d7d29a2805788542dea3833a17
Georg <gwi@tu-clausthal.de>
2014-09-03 15:28:28 +0200

    cowsmod

--- Implementation/Prototypes/MasterRindApp/app/models/cowsMod.js
+++ Implementation/Prototypes/MasterRindApp/app/models/cowsMod.js
@@ -1,75 +1,69 @@
 exports.definition = {
 	config : {
 		columns : {
-			"id" : "text",
-			"name" : "text",
-			"image" : "image",
-			"info" : "text"
-			
-	 "m_a" : "text",
-	 "m_aAa" : "text",
-	 "m_mating" :"text",
-	var m_yearOfBirth = "null";
-	var m_monthOfBirth = "null";
-	var m_dayOfBirth = "null";
-	var m_geneDefect = "null";
-	//herdbuchnr
-	var m_herdBookNumber = "null";
-	var m_mmfName = "null";
-	var m_mmff = "null";
-	var m_mfName = "null";
-	var m_mff = "null";
-	var m_fmf = "null";
-	var m_ff = "null";
-	var m_fff = "null";
-	var m_father = "null";
-	var m_breeder = "null";
-	var m_image = "image";
-	var m_name = "null";
-	var m_race = "null";
-	var m_id = "null";
-	var m_gRZG = "null";
-	var m_gRZM = "null";
-	var m_milkKg = "null";
-	var m_fatPercent = "null";
-	var m_fatKg = "null";
-	var m_proteinPercent = "null";
-	var m_proteinKg = "null";
-	var m_gRZE = "null";
-	var m_milkType = "null";
-	var m_body = "null";
-	var m_base = "null";
-	var m_udder = "null";
-	var m_gRZFit = "null";
-	var m_gRZN = "null";
-	var m_gRZS = "null";
-	var m_gRZD = "null";
-	var m_gRZR = "null";
-	var m_gRZkd = "null";
-	var m_gRZKM = "null";
-	var m_calfRunningDirectly = "null";
 
-	var m_backLegsPosition = "null";
-	var m_beckTilt = "null";
-	var m_beckWide = "null";
-	var m_behindUdderHeight = "null";
-	var m_bodyDepth = "null";
-	var m_centralBelt = "null";
-	var m_dairyCharacter = "null";
-	var m_directionOfGrowthBack = "null";
-	var m_directionOfGrowthFront = "null";
-	var m_DirectionOfGrowthLength = "null";
-	var m_foreUdderSuspension = "null";
-	var m_hock = "null";
-	var m_movement = "null";
-	var m_size = "null";
-	var m_stealAngle = "null";
-	var m_strength = "null";
-	var m_udderDepth = "null";
-	var m_hornless = "null";
-	var m_genomics = "null";
-	var m_sexed = "null";
-	var m_freshSemen = "null";
+			"a" : "text",
+			"aAa" : "text",
+			"mating" : "text",
+			"yearOfBirth" : "text",
+			"monthOfBirth" : "text",
+			"dayOfBirth" : "text",
+			"geneDefect" : "text",
+			"herdBookNumber" : "text",
+			"mmfName" : "text",
+			"mmff" : "text",
+			"mfName" : "text",
+			"mff" : "text",
+			"fmf" : "text",
+			"ff" : "text",
+			"fff" : "text",
+			"father" : "text",
+			"breeder" : "text",
+			"image" : "image",
+			"name" : "text",
+			"race" : "text",
+			"id" : "text",
+			"gRZG" : "text",
+			"gRZM" : "text",
+			"milkKg" : "text",
+			"fatPercent" : "text",
+			"fatKg" : "text",
+			"proteinPercent" : "text",
+			"proteinKg" : "text",
+			"gRZE" : "text",
+			"milkType" : "text",
+			"body" : "text",
+			"base" : "text",
+			"udder" : "text",
+			"gRZFit" : "text",
+			"gRZN" : "text",
+			"gRZS" : "text",
+			"gRZD" : "text",
+			"gRZR" : "text",
+			"gRZkd" : "text",
+			"gRZKM" : "text",
+			"calfRunningDirectly" : "text",
+			"backLegsPosition" : "text",
+			"beckTilt" : "text",
+			"beckWide" : "text",
+			"behindUdderHeight" : "text",
+			"bodyDepth" : "text",
+			"centralBelt" : "text",
+			"dairyCharacter" : "text",
+			"directionOfGrowthBack" : "text",
+			"directionOfGrowthFront" : "text",
+			"DirectionOfGrowthLength" : "text",
+			"foreUdderSuspension" : "text",
+			"hock" : "text",
+			"movement" : "text",
+			"size" : "text",
+			"stealAngle" : "text",
+			"strength" : "text",
+			"udderDepth" : "text",
+			"hornless" : "text",
+			"genomics" : "text",
+			"sexed" : "text",
+			"freshSemen" : "text"
 		},
 		adapter : {
 			// type : "sql",
